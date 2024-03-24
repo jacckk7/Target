2e8c9b793d78345d7b12011638565eaca789bd42
Breno <brenocal73@gmail.com>
2024-03-24 15:39:40 -0300

    Feitas as questões.

---
+++ quinta.py
@@ -0,0 +1,7 @@
+frase = input("Escreva uma frase ou palavra para ser invertida:\n")
+frase_invertida = ''
+
+for i in range(len(frase)):
+  frase_invertida = frase[i] + frase_invertida
+  
+print(frase_invertida)
---
+++ segunda.py
@@ -0,0 +1,21 @@
+def is_fibonacci(numero):
+  if numero == 0 or numero == 1:
+    return True
+  
+  x = 0
+  y = 1
+  
+  while y < numero:
+    x, y = y, x + y
+
+  if y == numero:
+    return True
+  else:
+    return False
+
+numero = int(input("Digite um número:\n"))
+
+if is_fibonacci(numero):
+    print(f"O número {numero} pertence à sequência de Fibonacci.")
+else:
+    print(f"O número {numero} não pertence à sequência de Fibonacci.")
